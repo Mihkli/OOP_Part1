9af8c0c75bff16cc184d559059461156ac88255f
Opilane TTHK <opilane@tthk.ee>
2021-11-24 13:06:04 +0200

    first commit

---
+++ OOPCat/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace OOPCat
+{
+    class Program
+    {
+        class Cat
+        {
+            string name;
+            string color;
+            double hungriness;
+
+            public Cat(string _name, string _color)
+            {
+                name = _name;
+                color = _color;
+                hungriness = 0;
+                Console.WriteLine($"A cat named {name} has appeared!");
+            }
+            public string Name //Name getter
+            {
+                get { return name; }
+            }
+            public string Color
+            {
+                get { return color; }
+            }
+            public double Hungriness
+            {
+                get { return hungriness; }
+            }
+            public void Sleep()
+            {
+                Console.WriteLine("*Sleeps*");
+                hungriness += 0.2; 
+            }
+            public void showcatdata()
+            {
+                Console.WriteLine($"Name: {Name}");
+                Console.WriteLine($"Color: {Color}");
+                Console.WriteLine($"Level of Hungriness: {Hungriness}");
+            }
+            public void Meow()
+            {
+                Console.WriteLine($"{Name} meows.");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Cat mycat = new Cat("Tiff", "Black");
+            
+            while(mycat.Hungriness != 1)
+            {
+                mycat.Sleep();
+            }
+            mycat.Meow();
+            Console.WriteLine($"Do u wish the see the cats properties. If yes press '.', if no press 'n'");
+            string userinput = Console.ReadLine();
+            if (userinput == ".")
+            {
+                mycat.showcatdata();
+            }
+            else
+            {
+                mycat.Meow();
+            }
+        }
+    }
+}
---
+++ OOP_PartI/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace OOP_PartI
+{
+    class Program
+    {
+        class Dog
+        {
+            string name;
+            int spots;
+            double happiness;
+
+            public Dog(string _name, int _spots) //constructor
+            {
+                name = _name;
+                spots = _spots;
+                happiness = 0;
+                Console.WriteLine($"The dog {name} has been created.");
+
+            }
+
+            //getters
+            public string Name //Name getter
+            {
+                get { return name; }
+            }
+            public int Spots
+            {
+                get { return spots; }
+            }
+            public double Happiness
+            {
+                get { return happiness; }
+            }
+            public void Barks()
+            {
+                Console.WriteLine("Woof-woof!");
+                happiness += 0.2; //happiness = happiness + 0.2
+            }
+            public void Wags()
+            {
+                Console.WriteLine($"{Name}: Wiggle-wiggle");
+            }
+
+            public void Rename(string newName)
+            {
+                name = newName;
+            }
+            public void showdogdata()
+            {
+                Console.WriteLine($"Name: {Name}");
+                Console.WriteLine($"Spots: {Spots}");
+                Console.WriteLine($"Level of happiness: {Happiness}");
+            }
+        }
+        
+
+        static void Main(string[] args)
+        {
+            Dog mydog = new Dog("Sulg", 15);
+            mydog.Barks();
+            Console.WriteLine($"{mydog.Name}'s level of happiness: {mydog.Happiness}");
+            Dog newdog = new Dog("Piirut", 2);
+            Console.WriteLine($"{newdog.Name}'s level of happiness: {newdog.Happiness}");
+
+            while(mydog.Happiness != 1)
+            {
+                mydog.Barks();
+            }
+            mydog.Wags();
+
+            Console.WriteLine($"Enter a new name for the dog '{mydog.Name}'");
+            string userinput = Console.ReadLine();
+            mydog.Rename(userinput);
+            mydog.showdogdata();
+        }
+    }
+}
